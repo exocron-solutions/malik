8e24b8639f56652500b2784ef4bd465207e2b35a
Felix Klauke <info@felix-klauke.de>
2018-06-18 08:09:06 +0200

    Add service layer and relocate controller to proper package.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

---
+++ server/src/main/java/de/exocronsolutions/malik/server/controller/MalikRestController.java
@@ -0,0 +1,27 @@
+package de.exocronsolutions.malik.server.controller;
+
+import de.exocronsolutions.malik.core.twitter.BlockList;
+import de.exocronsolutions.malik.server.blocklist.BlockListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Felix Klauke <info@felix-klauke.de>
+ */
+@RestController
+public class MalikRestController {
+
+    private final BlockListRepository blockListRepository;
+
+    @Autowired
+    public MalikRestController(BlockListRepository blockListRepository) {
+        this.blockListRepository = blockListRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/twitter/blocklist")
+    public BlockList getBlockList() {
+        return blockListRepository.find();
+    }
+}
--- server/src/main/java/de/exocronsolutions/malik/server/service/BlockListService.java
+++ server/src/main/java/de/exocronsolutions/malik/server/service/BlockListService.java
@@ -1,26 +1,23 @@
-package de.exocronsolutions.malik.server;
+package de.exocronsolutions.malik.server.service;
 
 import de.exocronsolutions.malik.core.twitter.BlockList;
 import de.exocronsolutions.malik.server.blocklist.BlockListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Felix Klauke <info@felix-klauke.de>
  */
-@RestController
-public class MalikRestController {
+@Service
+public class BlockListService {
 
     private final BlockListRepository blockListRepository;
 
     @Autowired
-    public MalikRestController(BlockListRepository blockListRepository) {
+    public BlockListService(BlockListRepository blockListRepository) {
         this.blockListRepository = blockListRepository;
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/twitter/blocklist")
     public BlockList getBlockList() {
         return blockListRepository.find();
     }
